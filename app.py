680fb6b3d3539c2b650a0406e1904676bfa5622a
JISHUBISHI <agnikbishi28@gmail.com>
2025-02-05 19:41:24 +0530

    initial commit

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ model_path = "housing_model.pkl"
 with open(model_path, "rb") as file:
     model = pickle.load(file)
 
-st.title("Housing Price Prediction")
+st.title("House Price Prediction")
 
 st.sidebar.header("Input Features")
 sqft = st.sidebar.number_input("Square Feet", min_value=500, max_value=10000)
 parking = st.sidebar.number_input("parking", min_value=0, max_value=5)
-heat_air_conditioning = st.sidebar.number_input("heat_air_conditioning", min_value=0, max_value=2)
-Total_rooms = st.sidebar.number_input("Totalrooms(including Bedrooms,Bathrooms,guestrooms)", min_value=0, max_value=10, value = 0)
+Total_rooms = st.sidebar.number_input("Totalrooms(including Bedrooms,Bathrooms,guestrooms)", min_value=0, max_value=15, value = 0)
 Extra_features = st.sidebar.selectbox("Furnish Status", ["furnished", "unfurnished", "semi-furnished"])
-
+heat_air_conditioning = st.sidebar.selectbox("Heating_And_Air-conditioning", ["NA", "Both", "Air-conditioning", "Heater"])
+heat_air_conditioning_mapping = {"NA": 0, "Both": 2, "Air-conditioning":1,"Heater":1}
 Extra_features_mapping = {"furnished": 1, "unfurnished": 3, "semi-furnished": 2}
 Extra_features_encoded = Extra_features_mapping [Extra_features]
-input_data = np.array([[sqft,parking,Extra_features_encoded,heat_air_conditioning,Total_rooms]])
+heat_air_conditioning_encoded = heat_air_conditioning_mapping [heat_air_conditioning]
+input_data = np.array([[sqft,parking,Extra_features_encoded,heat_air_conditioning_encoded,Total_rooms]])
 
 if st.button("Predict Price"):
     prediction = model.predict(input_data)[0]
     st.success(f"Predicted Price: ${prediction:,.2f}")
-
-# git branch -M main
-# git remote add origin https://github.com/JISHUBISHI/ai_recipie_generadadd.git
-# git push -u origin main
